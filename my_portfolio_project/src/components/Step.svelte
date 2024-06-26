296253fbfb795052fab3a07c58a223a8ee9dabc6
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-06-26 07:35:30 +0300

    div correction, it had a writing error

--- my_portfolio_project/src/components/Step.svelte
+++ my_portfolio_project/src/components/Step.svelte
@@ -1,21 +1,28 @@
 <script>
+    // Viedään komponentin "step" prop
     export let step;
 </script>
 
+<!-- Ankkurilinkki, joka ohjaa käyttäjän step.href arvoon -->
 <a
     href={step.href}
     target="_blank"
     class="p-4 sm:p-6 md:p-8 flex flex-col gap-4 rounded-lg border border-solid border-violet-700 text-center cursor-pointer group hover:border-violet-400 duration-200"
 >
+    <!-- Kuvake -->
     <div
         class="bg-slate-950 grid place-items-center px-4 text-5xl md:text-6xl -mt-10 sm:-mt-12 md:-mt-14 lg:-mt-16 mx-auto duration-200"
     >
+        <!-- Käytetään step.icon arvoa kuvakkeen näyttämiseen -->
         <i class={step.icon} />
     </div>
+    <!-- Projektin nimi -->
     <h3 class="font-medium text-xl sm:text-2xl md:text-3xl">
         {step.name}
     </h3>
+    <!-- Paikka lisäsisällölle -->
     <slot />
+    <!-- Linkki projektin sivulle -->
     <div class="flex-1 flex justify-betweeen gap-4 items-end">
         <div
             class="ml-auto cursor-pointer hover:text-slate-950 duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:bg-white after:w-full after:h-full after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden"
