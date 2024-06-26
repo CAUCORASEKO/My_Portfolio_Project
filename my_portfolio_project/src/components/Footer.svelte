09ee2da2b05f2ae1ce7958816fcd9a1c5f9d0fe2
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-06-26 07:42:24 +0300

    Some minor corrections in the code, some div writing errors.

--- my_portfolio_project/src/components/Footer.svelte
+++ my_portfolio_project/src/components/Footer.svelte
@@ -1,13 +1,19 @@
+<!-- Footer-osa -->
 <footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
+    <!-- Yhteydenottokutsu -->
     <p class="px-4 py-2 bg-white text-slate-950 font-medium">
         Connect with me;
     </p>
+    <!-- Yhteystiedot -->
     <div class="flex flex-col gap-4 items-center justify-center">
+        <!-- Sähköpostiosoite -->
         <p><b class="pr-2">Email</b> cauco_dev@gmail.com</p>
+        <!-- GitHub-linkki -->
         <p>
             <b class="pr-2">GitHub</b>
             <a href="https://github.com/CAUCORASEKO" target="_blank" class="text-violet-400">
                 CAUCO
+                <!-- Ikoni, joka osoittaa, että linkki avautuu uuteen välilehteen -->
                 <sup>
                     <span class="text-xs scale-75 pl-0.5">
                         <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
@@ -15,10 +21,12 @@
                 </sup>
             </a>
         </p>
+        <!-- LinkedIn-linkki -->
         <p>
             <b class="pr-2">LinkedIn</b>
             <a href="https://www.linkedin.com/in/multimedia3d/" target="_blank" class="text-violet-400">
                 CAUCO
+                <!-- Ikoni, joka osoittaa, että linkki avautuu uuteen välilehteen -->
                 <sup>
                     <span class="text-xs scale-75 pl-0.5">
                         <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
--- my_portfolio_project/src/components/Header.svelte
+++ my_portfolio_project/src/components/Header.svelte
@@ -1,6 +1,8 @@
 <script>
+    // Viedään komponentin "y" prop, joka mittaa scrollauksen määrää
     export let y;
 
+    // Määritellään navigointipalkin välilehdet ja niiden linkit
     export let tabs = [
         { name: "Projects", link: "#projects" },
         { name: "About me", link: "#about" },
@@ -8,17 +10,22 @@
     ];
 </script>
 
+<!-- Header-komponentti, joka on kiinnitetty sivun yläreunaan -->
 <header
     class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid " +
         (y > 0
             ? " py-4 bg-slate-950 border-violet-950"
             : " py-6 bg-transparent border-transparent")}
 >
+    <!-- Nimi ja titteli -->
     <h1 class="font-medium">
         <b class="font-bold poppins">Claudio</b> <span class="">Valenzuela</span>
     </h1>
+
+    <!-- Navigointilinkit -->
     <div class="sm:flex items-center gap-4 hidden">
         {#each tabs as tab, index}
+            <!-- Jokainen välilehti -->
             <a
                 href={tab.link}
                 class="duration-200 hover:text-violet-400"
@@ -27,7 +34,8 @@
                 <p>{tab.name}</p>
             </a>
         {/each}
-        
+
+        <!-- Yhteydenottopainike -->
         <button
             class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
         >
