d82ee87ff77de3bd0c61aeb6aa7753f71278ca6b
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-06-26 09:02:47 +0300

    corrections to comments, writing errors in the Finnish language

--- my_portfolio_project/src/routes/+layout.svelte
+++ my_portfolio_project/src/routes/+layout.svelte
@@ -1,39 +1,51 @@
 <script>
-// @ts-nocheck
-
-  import Footer from "../components/Footer.svelte";
-  import Header from "../components/Header.svelte";
-  import "../app.css";
-  /**
-     * @type {number}
-     */
-  let y;
-  let innerWidth = 0;
-  let innerHeight = 0;
-
-  function goTop() {
-      document.body.scrollIntoView();
-  }
-</script>
-
-<div
-  class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
->
+  // @ts-nocheck
+  
+    // Tuodaan Footer ja Header komponentit
+    import Footer from "../components/Footer.svelte";
+    import Header from "../components/Header.svelte";
+    import "../app.css"; // Tuodaan CSS-tiedosto
+  
+    // Määritellään muuttujat
+    /**
+       * @type {number}
+       */
+    let y;
+    let innerWidth = 0;
+    let innerHeight = 0;
+  
+    // Funktio, joka scrollaa sivun yläreunaan
+    function goTop() {
+        document.body.scrollIntoView();
+    }
+  </script>
+  
+  <!-- Pääsäilö, joka sisältää sivun rakenteen -->
   <div
-      class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
-          (y > 0
-              ? " opacity-full pointer-events-auto"
-              : " pointer-events-none opacity-0")}
+    class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
   >
-      <button
-          on:click={goTop}
-          class="ml-auto rounded-full aspect-square bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
-      >
-          <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
-      </button>
+    <!-- Painike, joka scrollaa sivun yläreunaan -->
+    <div
+        class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
+            (y > 0
+                ? " opacity-full pointer-events-auto"
+                : " pointer-events-none opacity-0")}
+    >
+        <button
+            on:click={goTop}
+            class="ml-auto rounded-full aspect-square bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
+        >
+            <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
+        </button>
+    </div>
+    <!-- Tuodaan Header komponentti ja välitetään muuttujat -->
+    <Header {y} {innerHeight}/>
+    <!-- Sisältö -->
+    <slot />
+    <!-- Tuodaan Footer komponentti -->
+    <Footer />
   </div>
-  <Header {y} {innerHeight}/>
-  <slot />
-  <Footer />
-</div>
-<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
+  
+  <!-- Svelte komponentti, joka sitoo scrollauksen ja ikkunan koon -->
+  <svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
+  
--- my_portfolio_project/src/routes/+page.svelte
+++ my_portfolio_project/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+    // Tuodaan Main-komponentti
     import Main from "../components/Main.svelte";
 </script>
 
+<!-- Renderoidaan Main-komponentti -->
 <Main />
